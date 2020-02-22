356fcedc33e85ae5135f06d43aa513011362c3c1
KANAK MALVIYA <60460750+Kanakmalviya04@users.noreply.github.com>
2020-02-22 11:37:48 +0530

    JS second

---
+++ JS study-II/1-Variables.js
@@ -0,0 +1,17 @@
+// Declare the number variable
+let number=1;
+
+// Print the value of the number variable, then add 1 to it
+console.log(number);
+number+=1;
+console.log(number);
+
+// Copy the 2 lines above and paste them 4 times below
+number+=1;
+console.log(number);
+number+=1;
+console.log(number);
+number+=1;
+console.log(number);
+number+=1;
+console.log(number);
---
+++ JS study-II/11-Accessing & Updating Values.js
@@ -0,0 +1,9 @@
+const character = {name: "Ken the Ninja", age: 14};
+
+// Print the value of the character's name property
+console.log(character.name);
+
+// Change the value of the character's age to 20 
+character.age=20;
+console.log(character);
+// Print the character constant in the console
