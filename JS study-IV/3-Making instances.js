fa90d95e9c921a2ec88a202d9d971f8f3b631864
KANAK MALVIYA <60460750+Kanakmalviya04@users.noreply.github.com>
2020-02-25 14:49:40 +0530

    JS fourth

---
+++ JS study-IV/3-Making instances.js
@@ -0,0 +1,11 @@
+class Animal {
+}
+
+// Assign the Animal class instance to the animal constant
+const animal=new Animal();
+
+// Output the value of the animal constant
+console.log(animal);            
+
+
+
